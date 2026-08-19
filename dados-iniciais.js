b54a6c7b39fec7574fbebf6b1bf0f4d459036772
gdesouza226-code <gdesouza226@gmail.com>
2026-08-19 15:32:42 -0300

    Add files via upload

---
+++ dados-iniciais.js
@@ -0,0 +1,33 @@
+window.DADOS_INICIAIS = {
+  "title": "Treinamentos",
+  "inspectors": [
+    {
+      "id": "gabriel-soares",
+      "name": "Gabriel Soares"
+    },
+    {
+      "id": "everton",
+      "name": "Everton"
+    },
+    {
+      "id": "gabriel-daniel",
+      "name": "Gabriel Daniel"
+    },
+    {
+      "id": "alexandre",
+      "name": "Alexandre"
+    }
+  ],
+  "trainings": [
+    {
+      "id": 1,
+      "inspectorId": "gabriel-soares",
+      "title": "Treinamentos",
+      "description": "Apresentação de orientações de segurança para lojistas.",
+      "file": "treinamento-exemplo.pptx",
+      "fileName": "APRESENTACAO_LOJISTAS_ LOGO NOVO.pptx",
+      "fileType": "PowerPoint",
+      "fileKey": null
+    }
+  ]
+};
